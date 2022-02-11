7abb2ed5521a16cda3c27e659be2da56cc3082b1
mihai.dumitrache96 <mihai.dumitrache96@hotmail.com>
2022-02-11 02:51:57 +0200

    Add project files.

---
+++ SAIOT_app/Form1.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(576, 388)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Panel1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(568, 362)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Comezi serial"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.Controls.Add(Me.ListBox1)
+        Me.Panel1.Location = New System.Drawing.Point(6, 6)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(575, 365)
+        Me.Panel1.TabIndex = 0
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {""})
+        Me.ListBox1.Location = New System.Drawing.Point(3, 3)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(559, 355)
+        Me.ListBox1.TabIndex = 1
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.TextBox2)
+        Me.TabPage2.Controls.Add(Me.Button1)
+        Me.TabPage2.Controls.Add(Me.RadioButton2)
+        Me.TabPage2.Controls.Add(Me.RadioButton1)
+        Me.TabPage2.Controls.Add(Me.TextBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(568, 362)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Schimbare locatie"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(283, 48)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(271, 33)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.Text = "Longitudine"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 113)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Trimite"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(113, 16)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(85, 17)
+        Me.RadioButton2.TabIndex = 2
+        Me.RadioButton2.Text = "Social media"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(6, 16)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(55, 17)
+        Me.RadioButton1.TabIndex = 1
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Vreme"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 48)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(271, 33)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Latitudine"
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox5.ErrorImage = Nothing
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.PictureBox5.InitialImage = Nothing
+        Me.PictureBox5.Location = New System.Drawing.Point(584, 3)
+        Me.PictureBox5.Margin = New System.Windows.Forms.Padding(8, 6, 8, 6)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(15, 15)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 152
+        Me.PictureBox5.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(603, 403)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ SAIOT_app/Form1.vb
@@ -0,0 +1,127 @@
+﻿Imports System.IO.Ports
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    Public Event ScanDataRecieved(ByVal data As String)
+    WithEvents comPort As SerialPort
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+        Application.Exit()
+        End
+    End Sub
+
+    Public Const WM_NCLBUTTONDOWN = &HA1
+    Public Const HTCAPTION = 2
+    Public Declare Sub ReleaseCapture Lib "user32" ()
+
+    Declare Function SendMessage Lib "user32" Alias "SendMessageA" (ByVal hwnd As Integer, ByVal wMsg As Integer, ByVal wParam As Integer, <MarshalAs(UnmanagedType.AsAny)> ByVal lParam As Object) As Integer
+
+    Private Sub Me_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        If e.Button = MouseButtons.Left Then
+            ReleaseCapture()
+            SendMessage(Me.Handle.ToInt32, WM_NCLBUTTONDOWN, HTCAPTION, 0&)
+        End If
+    End Sub
+
+    Sub TextBox1_GotFocus(sender As Object, e As EventArgs) Handles TextBox1.GotFocus
+        If TextBox1.Text = "Latitudine" Then TextBox1.Text = ""
+        If TextBox1.Text = "Cont" Then TextBox1.Text = ""
+    End Sub
+
+    Sub TextBox1_LostFocus(sender As Object, e As EventArgs) Handles TextBox1.LostFocus
+        If TextBox1.Text = "" Then
+            If RadioButton1.Checked Then TextBox1.Text = "Latitudine"
+            If RadioButton2.Checked Then TextBox1.Text = "Cont"
+        End If
+    End Sub
+    Sub TextBox2_GotFocus(sender As Object, e As EventArgs) Handles TextBox2.GotFocus
+        If TextBox2.Text = "Longitudine" Then TextBox2.Text = ""
+    End Sub
+
+    Sub TextBox2_LostFocus(sender As Object, e As EventArgs) Handles TextBox2.LostFocus
+        If TextBox2.Text = "" Then TextBox2.Text = "Longitudine"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "Latitudine" Then GoTo out
+        If TextBox2.Visible AndAlso TextBox2.Text = "Longitudine" Then GoTo out
+        If TextBox1.Text = "Cont" Then GoTo out
+        If TextBox1.Text = "" Then GoTo out
+        Dim infoString As String = TextBox1.Text
+        If RadioButton1.Checked Then
+            If IsNumeric(infoString) AndAlso IsNumeric(TextBox2.Text) Then
+                infoString = infoString & " " & TextBox2.Text
+            Else
+                MsgBox("Ambele campuri trebuie sa fie valori numerice")
+                GoTo out
+            End If
+        End If
+        MsgBox(postRequest(routes.weatherForecast, "{""isWeather"": " & RadioButton1.Checked.ToString.ToLower & ", ""info"": """ & infoString & """}"))
+
+out:
+    End Sub
+
+
+
+    Public Sub Connect()
+        Try
+            comPort = My.Computer.Ports.OpenSerialPort("COM5", 9600)
+        Catch
+        End Try
+    End Sub
+
+    Public Sub Disconnect()
+
+        If comPort IsNot Nothing AndAlso comPort.IsOpen Then
+            comPort.Close()
+        End If
+
+    End Sub
+
+    Private Sub comPort_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles comPort.DataReceived
+        On Error Resume Next
+        Dim str As String = ""
+        If e.EventType = SerialData.Chars Then
+            Do
+                Dim bytecount As Integer = comPort.BytesToRead
+
+                If bytecount = 0 Then
+                    Exit Do
+                End If
+                Dim byteBuffer(bytecount) As Byte
+
+                comPort.Read(byteBuffer, 0, bytecount)
+                str = str & System.Text.Encoding.ASCII.GetString(byteBuffer, 0, 1)
+                If str = "" Then Exit Do
+                Me.Invoke(Sub() ListBox1.Items.Add("Comanda primita: " & str))
+                Dim response As String = ""
+                Select Case str
+                    Case "wheather"
+                        response = getRequest(routes.weatherForecast & "/1")
+                    Case "account"
+                        response = getRequest(routes.weatherForecast & "/2")
+                End Select
+                If response <> "" Then
+                    For i As Integer = 0 To response.Length
+                        comPort.Write(response(i))
+                        System.Threading.Thread.Sleep(1)
+                    Next
+                End If
+            Loop
+        End If
+
+        RaiseEvent ScanDataRecieved(str)
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        TextBox2.Visible = RadioButton1.Checked
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Disconnect()
+    End Sub
+End Class
